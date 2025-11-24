ca4eb63eac29e3f9823412098f0752ed45ba7c3c
Mert Atmaca <157486191+Growlitthe@users.noreply.github.com>
2025-11-24 16:57:43 +0300

    Update 0169-majority-element.py

--- leetcode/arrays/0169-majority-element.py
+++ leetcode/arrays/0169-majority-element.py
@@ -1,1 +1,50 @@
+#Majority Element - Dictionary ile çözüm
+'''def majorityElement(nums: list[int]) -> int:
+    sozluk = {}
+
+    for i in nums:
+        if i in sozluk.keys():
+            sozluk[i] +=1
+        else:
+            sozluk[i] = 1
+
+    enBuyuk = 0
+
+    for key, value in sozluk.items():
+        if value > enBuyuk:
+            enBuyuk = value
+            sonuc = key
+    return sonuc
+
+liste = [3,2,3,2,2,3,3,3,2,2,2,2,2,2,2,2,2]
+print(majorityElement(liste))'''
+
+
+#Majority Element - Boyer-Moore Voting ile çözüm
+'''def majorityElement(nums: list[int]) -> int:
+    aday = 0
+    sayac = 0
+
+    for i in nums:
+
+        if sayac == 0:
+            aday = i
+
+        if aday == i:
+            sayac += 1
+        else:
+            sayac -= 1
+    return aday
+
+liste = [3,2,3,2,2]
+print(majorityElement(liste))'''
+
+
+
+
+
+
+
+
+
 
